7023a19942005b077f59ceb077e2bd6e151096fa
Satyam-Pandey677 <satyampandey0241@gmail.com>
2026-08-04 00:53:46 +0530

    fix: add missing console log for PORT environment variable

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { uploadController } from "./controller/imageController.js";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
+console.log(process.env.PORT)
 app.get("/", (req, res) => {
    return res.status(200).json({
     message:"Hello server is running"
